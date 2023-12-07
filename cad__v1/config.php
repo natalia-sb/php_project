865dd31d0b4d09314acd11e4a88a9e1e7bc8084d
Natália Batista <68193123+natalia-sb@users.noreply.github.com>
2023-12-07 16:03:30 -0300

    Update config.php

--- cad__v1/config.php
+++ cad__v1/config.php
@@ -6,17 +6,17 @@ $db = 'cadastro';
 
 $conexao = new mysqli($host, $user, $pass, $db);
 
-//verifica se o formulário foi enviado
+//coleta os dados do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //coleta os dados do formulário
     $nome = $_POST["Nome"];
     $telefone = $_POST["telefone"];
     $email = $_POST["email"];
     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
 
-    //prepara e executa a instrução SQL
+    //inserção SQL
     $sql = "INSERT INTO new_cadastro (Nome, Telefone, Email, Senha) VALUES ('$nome', '$telefone', '$email', '$senha')";
 
+    //verifica se registro foi inserido
     if ($conexao->query($sql) === TRUE) {
         echo "Registro inserido com sucesso!";
     } else {
